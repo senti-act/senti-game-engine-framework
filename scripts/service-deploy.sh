19b415bab05497d6df4f9b492cbf64c6fa3fa616
Andrei Tudor <andrei.tudor27@gmail.com>
2020-02-18 14:18:16 +0100

    disable register service

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -6,7 +6,7 @@ if [[ "$1" == "master" ]]; then
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/act/game-engine/production
 	echo
 	echo Restarting Senti.Act Game Engine Framework service: $1 ...
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/act/game-engine/production/registerservice-production'
+	# ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/act/game-engine/production/registerservice-production'
 	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/act/game-engine/production/scripts/service-restart.sh master'
 	echo
 	echo Deployment to Senti.Act Game Engine Framework $1 and restart done!
@@ -19,7 +19,7 @@ if [[ "$1" == "dev" ]]; then
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/act/game-engine/development
 	echo
 	echo Restarting Senti.Act Game Engine Framework service: $1 ...
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/act/game-engine/development/registerservice-development'
+	# ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/act/game-engine/development/registerservice-development'
 	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/act/game-engine/development/scripts/service-restart.sh dev'
 	echo
 	echo Deployment to Senti.Act Game Engine Framework $1 and restart done!
