04630a0baad9558ff3587547cdfa464c3600984b
Karol Piernikarz <piernik@MacBook-Pro-Karol.local>
2020-04-29 00:16:16 +0200

    usage by day

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ var userRepository = require('../repository/userRepository');
 var Repo = new userRepository();
 const uuidv4 = require('uuid/v4');
 const moment = require('moment');
+const axios = require("axios");
 
 //get all users
 router.get('/', (req, res) => {
@@ -55,5 +56,28 @@ router.post('/', (req, res) => {
     })
 })
 
+//get user by uuid
+router.get('/usageByDay/:startDate/:endDate', (req, res) => {
+    var token=req.header('Authorization');
+    var tokenSegments=token.split(' ');
+    var actualToken=tokenSegments[1];
+     
+    getUsageByDay(req.params.startDate,req.params.endDate,actualToken).then(x=>{
+        res.status(200).json(x);
+    }).catch(x=>{
+        res.status(500).json(x);
+    })
+   
+})
 
+function getUsageByDay(startDate,endDate,token) {
+    return new Promise((resolve, reject) => {
+        const requestUri = `https://dev.services.senti.cloud/databroker/v2/waterworks/data/usagebyday/${startDate}/${endDate}`;
+        axios.get(requestUri, {headers :{ 'Authorization': 'Bearer ' + token}}).then(x => {
+            resolve(x.data);
+        }).catch(error => {
+            reject(error);
+        });         
+    });
+}
 module.exports = router;
